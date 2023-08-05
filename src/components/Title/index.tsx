9a18bb0a4b1f927315fcd19c0baa250bacd4c4dd
Pablo Viana <pabloviana2010@gmail.com>
2023-08-05 17:34:09 -0300

    refactor: fixed background and title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -5,9 +5,9 @@ type TitleFont = {
 }
 export const Title = ({font}: TitleFont) => {
     return (
-        <div className="md:max-w-[360px] max-w-[1280px] mx-auto">
+        <div className="sm:max-w-[370px] sm:p-6 md:max-w-[650px] 2xl:p-8 max-w-[1280px] mx-auto">
             <h1
-                className={`md:max-w-[360px] md:text-[40px] max-w-[670px] absolute border-2 top-[25%] text-7xl mt-16 p-10 uppercase text-[#fff] font-light ${font.className}`}
+                className={`sm:max-w-[320px] sm:p-6 md:max-w-[550px] md:text-[40px] max-w-[670px] absolute border-2 top-[25%] text-7xl mt-16 p-10 uppercase text-[#fff] font-light ${font.className}`}
             >
                 Immersive experiences that deliver
             </h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,7 @@ import { josefin } from '@/utils/fonts';
 
 export default function Home() {
   return (
-      <main className="w-full h-[650px] relative">
-          <Image
-              src={heroBackground}
-              className="w-full h-full"
-              alt="hero background"
-          />
+      <main className={`w-full h-[650px] relative bg-[url('../images/Hero.jpg')] bg-cover bg-fixed bg-center`}>
           <Header />
           <Title font={josefin}/>
       </main>
