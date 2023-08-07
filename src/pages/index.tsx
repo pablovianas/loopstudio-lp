7ce5914ca5dd74c747a2f4ab1fb03e6f1d422b57
Pablo Viana <pabloviana2010@gmail.com>
2023-08-07 11:45:49 -0300

    refactor: updated index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import { Josefin_Sans } from 'next/font/google'
-import heroBackground from '@/images/Hero.jpg'
-import Image from 'next/image';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
 import { josefin } from '@/utils/fonts';
+import { Interactive } from '@/components/Interactive';
 
 export default function Home() {
   return (
-      <main className={`w-full h-[650px] relative bg-[url('../images/Hero.jpg')] bg-cover bg-fixed bg-center`}>
-          <Header />
-          <Title font={josefin}/>
-      </main>
+      <>
+        <main className={`w-full h-[650px] relative bg-[url('../images/Hero.jpg')] bg-cover bg-fixed bg-center`}>
+            <Header />
+            <Title font={josefin}/>
+        </main>
+        <Interactive/ >
+      </>
   );
 }
